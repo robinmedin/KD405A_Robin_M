8e4ddf259bb35db6b583a4b86c9d24f3cecb5b15
robinmedin <robinmedin@telia.com>
2016-02-19 10:33:42 +0100

    blabla

--- Assignment_5/src/se/mah/k3/robin/ClockLogic.java
+++ Assignment_5/src/se/mah/k3/robin/ClockLogic.java
@@ -1,20 +1,66 @@
 package se.mah.k3.robin;
 
-public class ClockLogic extends DigitalClockGUI {
-	
-	
-	private DigitalClockGUI ClockLogic;
-	private int alarmHour;
-	private int alarmMinute;
-	
-	public ClockLogic (DigitalClockGUI DigitalClockGUI){	
-	}
-	
-	public void setAlarm(int hours, int minute){
-	}
-	
-	public void clearAlarm(){		
-	}
-	
+import java.util.Date;
+import java.util.GregorianCalendar;
+import java.text.DateFormat;
+import java.text.DecimalFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 
+public class ClockLogic {
+
+	private DigitalClockGUI clockGUI;
+	private int alarmHour = -1;
+	private int alarmMinute = -1;
+
+	public ClockLogic(DigitalClockGUI DigitalClockGUI) {
+		this.clockGUI = DigitalClockGUI;
+		new ClockThread().start();
+	}
+
+	public void setAlarm(int hour, int minute) {
+		this.alarmHour = hour;
+		this.alarmMinute = minute;
+		System.out.println("Set");
+	}
+
+	public void clearAlarm() {
+		this.alarmHour = -1;
+		this.alarmMinute = -1;
+		System.out.println("Clear");
+	}
+
+	public class ClockThread extends Thread {
+
+		Thread time = new Thread();
+
+		@Override
+
+           public void run() {
+               DecimalFormat timeFormat = new DecimalFormat("00");
+               while(true) {
+                   
+                   try {
+                       Thread.sleep(1000);
+                       } catch (InterruptedException e) {} 
+                   System.out.println("Starting");
+                   Calendar cal = Calendar.getInstance();
+                   int day = cal.get(Calendar.DAY_OF_MONTH);
+                   int month = cal.get(Calendar.MONTH);
+                   int year = cal.get(Calendar.YEAR);
+                   
+                   int second = cal.get(Calendar.SECOND);
+                   int minute = cal.get(Calendar.MINUTE);
+                   int hour = cal.get(Calendar.HOUR_OF_DAY);
+                   clockGUI.setTimeOnLabel(timeFormat.format(hour)+":"+timeFormat.format(minute)+":"+timeFormat.format(second));
+                   
+           //        If(hour == alarmHour) { 
+           //     	   DigitalClockGUI.activateAlarm(true);
+
+                   //}
+               
+               }
+			
+		}
+	}
 }
--- Assignment_5/src/se/mah/k3/robin/DigitalClockGUI.java
+++ Assignment_5/src/se/mah/k3/robin/DigitalClockGUI.java
@@ -6,22 +6,48 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import java.awt.Font;
+import java.awt.Color;
+import javax.swing.JButton;
+import javax.swing.ImageIcon;
+import javax.swing.JTextField;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class DigitalClockGUI extends JFrame {
 
 	private JPanel contentPane;
 	private ClockLogic clockLogic;
+	private JLabel clockFace;
+	private final JLabel lblHour = new JLabel("Hour:");
+	private JTextField textFieldHour;
+	private JTextField textFieldMin;
+	private JLabel lblAlarmmessage;
+	private JLabel lblAlarmAt;
+	public boolean activate;
 		
-	public void DigitalClockGUI(){
+	public void DigitalClockGUI() {
 	}
 	
-	public void setTimeOnLabel(String time){	
+	public void setTimeOnLabel(String time){
+		clockFace.setText(time);
+		
 	}
 	
-	public void activateAlarm(boolean activate){	
-	}
-	
-	
+	    
+	    public void setAlarmOnLabel(String alarmTime){
+	        lblAlarmAt.setText(alarmTime);
+	    }
+	    
+	    public void activateAlarm(boolean activate){   
+	        if(activate)
+	        {
+	            lblAlarmmessage.setText("I COMMAND YOU TO WAKE UP");
+	        }else{
+	            lblAlarmmessage.setText(" ");
+	        }
+	    }
 	
 
 	/**
@@ -47,9 +73,79 @@ public class DigitalClockGUI extends JFrame {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
+		contentPane.setBackground(Color.BLACK);
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		//URTAVLAN
+		clockFace = new JLabel("00:00:00");
+		clockFace.setForeground(new Color(255, 0, 0));
+		clockFace.setFont(new Font("Synchro LET", Font.PLAIN, 60));
+		clockFace.setBounds(105, 25, 246, 73);
+		contentPane.add(clockFace);
+		
+		JButton btnSetAlarm = new JButton("Set");
+		btnSetAlarm.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				
+                String hourString = textFieldHour.getText();
+                int hour = Integer.parseInt(hourString);
+                String minuteString = textFieldMin.getText();
+                int minute = Integer.parseInt(minuteString);
+                clockLogic.setAlarm(hour, minute);
+				
+				lblAlarmAt.setText("Alarm at: " + textFieldHour.getText() + " : " + textFieldMin.getText());
+			}
+		});
+		btnSetAlarm.setBounds(369, 231, 61, 29);
+		contentPane.add(btnSetAlarm);
+		
+		JButton btnClearAlarm = new JButton("Clear");
+		btnClearAlarm.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				
+				clockLogic.clearAlarm();
+                lblAlarmAt.setText(" ");
+			}
+		});
+		btnClearAlarm.setBounds(306, 231, 61, 29);
+		contentPane.add(btnClearAlarm);
+		
+		clockLogic = new ClockLogic(this);
+		
+		JLabel lblAlarmAt = new JLabel("");
+		lblAlarmAt.setForeground(new Color(255, 0, 0));
+		lblAlarmAt.setFont(new Font("Synchro LET", Font.PLAIN, 15));
+		lblAlarmAt.setBounds(108, 90, 243, 29);
+		contentPane.add(lblAlarmAt);
+		lblHour.setForeground(new Color(255, 255, 255));
+		lblHour.setFont(new Font("Synchro LET", Font.PLAIN, 13));
+		lblHour.setBounds(21, 231, 46, 29);
+		contentPane.add(lblHour);
+		
+		JLabel lblMin = new JLabel("Min:");
+		lblMin.setForeground(Color.WHITE);
+		lblMin.setFont(new Font("Synchro LET", Font.PLAIN, 13));
+		lblMin.setBounds(120, 231, 46, 29);
+		contentPane.add(lblMin);
+		
+		textFieldHour = new JTextField();
+		textFieldHour.setFont(new Font("Synchro LET", Font.PLAIN, 13));
+		textFieldHour.setBounds(62, 233, 46, 26);
+		contentPane.add(textFieldHour);
+		textFieldHour.setColumns(10);
+		
+		textFieldMin = new JTextField();
+		textFieldMin.setFont(new Font("Synchro LET", Font.PLAIN, 13));
+		textFieldMin.setColumns(10);
+		textFieldMin.setBounds(150, 233, 46, 26);
+		contentPane.add(textFieldMin);
+		
+		JLabel lblNewLabel = new JLabel("New label");
+		lblNewLabel.setIcon(new ImageIcon("/Users/robinmedin/Desktop/KD405A_Robin_M/Assignment_5/src/se/mah/k3/robin/pics/background.png"));
+		lblNewLabel.setBounds(-65, -12, 515, 369);
+		contentPane.add(lblNewLabel);
+		
 	}
-
 }
